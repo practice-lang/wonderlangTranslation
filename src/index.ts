4dcd8d5b1a3cce20aed95969024ec947c743a74f
ttakkku <k2005118@gmail.com>
2019-06-08 20:48:01 +0900

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * @author ttakkku
+ * @version 1.0.0
+ * @license MIT
+ * @copyright Copyright (c) 2019 ttakkku
+ */
+
+const lang = require("wonderlangpark")
+
+let asy = process.argv[2];
+
+console.log(JSON.stringify(lang.towonder(asy)))
